58eac526209a5518b46fa066aa42b584a88f1e8d
Yash <yvc1705@gmail.com>
2025-02-26 04:52:06 +0530

    first commit

--- react-intro-kanban/src/App.tsx
+++ react-intro-kanban/src/App.tsx
@@ -1,29 +1,47 @@
+import { useState } from "react";
 import "./App.css";
 import TaskCard from "./components/TaskCard";
-import { tasks , statuses} from "./utils/data-tasks";
-
+import { tasks as initialTasks, statuses, Task } from "./utils/data-tasks";
 
 function App() {
+  const [tasks, setTasks] = useState<Task[]>(initialTasks);
   const columns = statuses.map((status) => {
     const tasksInColumn = tasks.filter((task) => task.status === status);
     return {
       title: status,
-      tasks: tasksInColumn
+      tasks: tasksInColumn,
     };
   });
-  
+
+  const updatedTaskPoints = (task: Task, points: number) => {
+    const updatedTasks = tasks.map((t) => {
+      return t.id === task.id ? { ...t, points } : t;
+    });
+    setTasks(updatedTasks);
+  };
 
   return (
-    <div className = 'flex divide-x'>
-      {columns.map((column)=>(  
-          <div>
-          <h2 className="text=3xl p-2 capitalize font-bold text-gray-500">{column.title}</h2>
-          {column.tasks.map((task)=> <TaskCard task = {task} /> )}
+    <div className="flex divide-x">
+      {columns.map((column) => (
+        <div>
+          <div className="flex justify-between task-3xl p2 font-bold text-gray-500">
+          <h2 className="text=3xl p-2 capitalize font-bold text-gray-500">
+            {column.title}
+          </h2>
+          <div className="text-xl p-2">
+            {column.tasks.reduce(
+              (total, task) => total + (task?.points || 0),
+              0
+            )}
           </div>
+          </div>
+          {column.tasks.map((task) => (
+            <TaskCard task={task} updatedTaskPoints={updatedTaskPoints} />
+          ))}
+        </div>
       ))}
     </div>
   );
 }
-
 
 export default App;
--- react-intro-kanban/src/components/TaskCard.tsx
+++ react-intro-kanban/src/components/TaskCard.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import { Task } from "../utils/data-tasks";
 
 const lowPriorityIcon = "🍃";
 const mediumPriorityIcon = "⚡";
 const highPriorityIcon = "🔥" ;
-const TaskCard = ({ task }: { task: Task }) => {
+const TaskCard = ({ task, updatedTaskPoints }: { 
+  task: Task 
+  updatedTaskPoints: (task:Task, points:number)=>void
+}) => {
+  const points = task.points || 0
+
   return (
-    <div className="text-4xl text border rounded-lg px-2 p-2 m-2 bg-grey-50 w-122">
+    <div className="text-4xl text border rounded-lg px-2 p-2 m-2 bg-grey-50 w-50">
       <div className="text-base font-semibold py-2">{task.title}</div>
       <div className="flex gap-4 justify-between py-2 text-gray-700 text-sm">
         <div className="flex gap-2">
@@ -15,7 +21,11 @@ const TaskCard = ({ task }: { task: Task }) => {
           {task.priority === "medium" && mediumPriorityIcon}
           {task.priority === "low" && lowPriorityIcon}
         </div>
-        <div>{task.points}</div>
+        <div className="flex gap-2 items-center">
+        <button onClick = {()=> updatedTaskPoints(task, points -1)}>-</button>
+        <div className="font-bold">{points}</div>
+        <button onClick={()=> updatedTaskPoints(task, points + 1) }>+</button>
+        </div>
       </div>
     </div>
   );
