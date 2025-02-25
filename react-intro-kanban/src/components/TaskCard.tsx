f8edf1c862ede65baf8aee8f64491d010780c2bf
Yash <yvc1705@gmail.com>
2025-02-26 03:46:29 +0530

    priority icons

--- react-intro-kanban/src/components/TaskCard.tsx
+++ react-intro-kanban/src/components/TaskCard.tsx
@@ -1,10 +1,20 @@
 import { Task } from "../utils/data-tasks";
+
+const lowPriorityIcon = "🍃";
+const mediumPriorityIcon = "⚡";
+const highPriorityIcon = "🔥" ;
 const TaskCard = ({ task }: { task: Task }) => {
   return (
     <div className="text-4xl text border rounded-lg px-2 p-2 m-2 bg-grey-50 w-122">
       <div className="text-base font-semibold py-2">{task.title}</div>
       <div className="flex gap-4 justify-between py-2 text-gray-700 text-sm">
-        <div>{task.id}</div>
+        <div className="flex gap-2">
+          <div>{task.id}</div>
+          <div>{task.priority}</div>
+          {task.priority === "high" && highPriorityIcon}
+          {task.priority === "medium" && mediumPriorityIcon}
+          {task.priority === "low" && lowPriorityIcon}
+        </div>
         <div>{task.points}</div>
       </div>
     </div>
--- react-intro-kanban/src/utils/data-tasks.tsx
+++ react-intro-kanban/src/utils/data-tasks.tsx
@@ -1,84 +1,95 @@
 export type Status = "todo" | "in-progress" | "done";
+export type Priority = "low" | "medium" | "high";
 export type Task = {
   title: string,
   id: string,
   status: Status,
+  priority: Priority
   points?: number,
 };
 
 
 export const statuses: Status[] = ["todo", "in-progress", "done"];
-
+export const priorities: Priority[] = ["low", "medium", "high"]; 
 
 export const tasks: Array<Task> = [
   {
     title: "Conduct User Interviews",
     id: "UX-1",
     status: "todo",
+    priority: "high",
     points: 9,
-},
+  },
 
-{
+  {
     title: "Develop Backend API",
     id: "DEV-2",
     status: "in-progress",
+    priority: "medium",
     points: 7,
-},
+  },
 
-{
+  {
     title: "Create Wireframe Designs",
     id: "UI-3",
     status: "done",
+    priority: "low",
     points: 4,
-},
+  },
 
-{
+  {
     title: "Write Blog Post",
     id: "CNT-4",
     status: "in-progress",
+    priority: "low",
     points: 3,
-},
+  },
 
-{
+  {
     title: "Plan Product Launch",
     id: "MKT-5",
     status: "todo",
+    priority: "high",
     points: 10,
-},
+  },
 
-{
+  {
     title: "Review Code Pull Request",
     id: "DEV-6",
     status: "done",
+    priority: "medium",
     points: 6,
-},
+  },
 
-{
+  {
     title: "Fix UI Bugs",
     id: "BUG-7",
     status: "in-progress",
+    priority: "high",
     points: 2,
-},
+  },
 
-{
+  {
     title: "Schedule Team Workshop",
     id: "MTG-8",
     status: "todo",
+    priority: "low",
     points: 5,
-},
+  },
 
-{
+  {
     title: "Update Product Roadmap",
     id: "PRD-9",
     status: "done",
+    priority: "medium",
     points: 8,
-},
+  },
 
-{
+  {
     title: "Conduct Performance Testing",
     id: "QA-10",
     status: "todo",
+    priority: "high",
     points: 1,
-}
-
+  }
 ];
